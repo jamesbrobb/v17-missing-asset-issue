4dabd49347f9317304d2fdf6e6c0021b342d766d
jamesbrobb <jamesbenrobb@gmail.com>
2024-01-22 12:00:45 +0000

    initial import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HttpClientModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'repro-app';
+  asset?: string;
+  loadError = false;
+
+  #http: HttpClient = inject(HttpClient);
+
+  loadAsset(src: string): void {
+    this.#http.get(`assets/${src}`, { responseType: 'text' }).subscribe({
+      next: (response: any) => {
+        this.asset = response;
+      },
+      error: () => {
+        this.loadError = true;
+      }
+    });
+  }
 }
